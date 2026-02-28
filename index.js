1c1722ecf699195302ca930720f18182d828584b
Nick Tominaga <nicktominaga@outlook.com>
2026-02-28 13:21:01 +0900

    Load serverConfig.jsonc via require hook

---
+++ index.js
@@ -0,0 +1,23 @@
+require("./jsonc-require");
+
+const express = require("express");
+const path = require("path");
+const serverConfig = require("./serverConfig.jsonc");
+
+const app = express();
+const publicDir = path.join(__dirname, "public");
+const port = serverConfig.port;
+
+if (typeof port !== "number") {
+  throw new Error("serverConfig.jsonc: 'port' must be a number.");
+}
+
+app.use(express.static(publicDir));
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(publicDir, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Nature Remo Web Controller server started: http://localhost:${port}`);
+});
